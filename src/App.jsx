2626a1c0ab61b9085169d192e32d30525ebe1105
Priyansh Khatri <priyanshkhatri8690@gmail.com>
2025-12-10 17:06:19 +0530

    started new project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SearchBar from "./components/searchBar"
+import WeatherCard from "./components/weatherCard"
+
+export default function App(){
+  return (
+    <>
+    <SearchBar/>
+    <WeatherCard/>
+    </>
+  )
+}
---
+++ src/components/weatherCard.jsx
@@ -0,0 +1,5 @@
+export default function WeatherCard(){
+return (
+    <div>priyanhs</div>
+)
+}
