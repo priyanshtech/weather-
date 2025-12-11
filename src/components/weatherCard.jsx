15c7451602717a6a6d085ff519596435f055d472
Priyansh Khatri <priyanshkhatri8690@gmail.com>
2025-12-11 15:45:43 +0530

    solved the error , when we type a random city

--- src/components/weatherCard.jsx
+++ src/components/weatherCard.jsx
@@ -1,13 +1,24 @@
 export default function WeatherCard({city,weather}){
+    if (!weather) {
+    return (
+      <div className="flex justify-center mt-10 text-xl text-red-600">
+        No city found. Try again.
+      </div>
+    );
+  }
+
+
+
+
 return (<div className="flex justify-center">
     <div className="mt-10 border-3 border-blue-900 h-fit w-fit px-3 py-3">
-        <h1 className="text-3xl   flex justify-center font-bold mb-5 ">Weather Report</h1>
+        <h1 className="text-3xl   flex justify-center font-bold mb-5 ">City Report</h1>
         <ul>
-        <li className="text-xl">City name{city} </li>
-        <li className="text-xl">Temperature {weather.naam}</li>
-        <li className="text-xl">Weather condition {weather.latitude}</li>
+        <li className="text-xl">City name:    {weather.naam} </li>
+        <li className="text-xl"> Timezone:  {weather?.timezone??"-"}</li>
+        <li className="text-xl">Latitude:                 {weather?.latitude??"-"}</li>
         
-        <li className="text-xl">Humidity{weather.timezone} </li>
+        <li className="text-xl">Population: {weather?.population??"-"} </li>
 
         </ul>
     </div></div>
